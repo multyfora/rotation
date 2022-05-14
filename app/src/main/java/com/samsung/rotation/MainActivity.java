57ba32c27dffdb2bb7562646173edc4ae217f312
Levon <aryuc06@gmail.com>
2022-05-14 15:19:39 +0400

    d

---
+++ app/src/main/java/com/samsung/rotation/MainActivity.java
@@ -0,0 +1,19 @@
+package com.samsung.rotation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Switch color;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
